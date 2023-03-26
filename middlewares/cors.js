20915a894899923504d7e7b46b977acf12264cc6
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-26 13:00:14 +0300

    http added to localhost in cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@
 const allowedCors = [
   'https://dorokhova.diploma.nomoredomains.work',
   'http://dorokhova.diploma.nomoredomains.work',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
